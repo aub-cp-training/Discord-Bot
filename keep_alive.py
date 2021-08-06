cc7f3232abf894760fd755ce3eb8edd67cafe8f4
aub-cp-training <88315952+aub-cp-training@users.noreply.github.com>
2021-08-06 13:44:06 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,14 @@
 from threading import Thread
 from flask import Flask
 
-ab = Flask('')
+app = Flask('')
 
 @ab.route('/')
 def main():
     return "Your bot is alive!"
 
 def run():
-    ab.run(host = "0.0.0.0", port = 8080)
+    app.run(host = "0.0.0.0", port = 8080)
 
 def keep_alive():
     server = Thread(target = run)
