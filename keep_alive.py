e456a33c1f72f0c6ce893f77c76436bf4ab1920d
Miguel Merheb <74906227+mrm-36@users.noreply.github.com>
2021-08-07 14:00:09 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def main():
     return "Your bot is alive!"
 
 def run():
-    ab.run(host = "0.0.0.0", port = 8080)
+    ab.run(host = "0.0.0.0", port = 5001)
 
 def keep_alive():
     server = Thread(target = run)
